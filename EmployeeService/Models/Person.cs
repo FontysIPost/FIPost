5ff7be214feff7bf7186d1f8b83d45c4853725f2
Giang <463189@student.fontys.nl>
2023-03-27 12:16:36 +0200

    refactor: getter and setter person

--- EmployeeService/Models/Person.cs
+++ EmployeeService/Models/Person.cs
@@ -2,10 +2,10 @@
 
 public class Person
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public string Email { get; init; }
     public string Password { get; set; }
     public int Role { get; set; }
-    public string FontysId { get; set; }
+    public string FontysId { get; }
 }
