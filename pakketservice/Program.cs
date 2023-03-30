e3a5b851501ffca9c9f91145012dab71f98249a7
Giang <463189@student.fontys.nl>
2023-03-30 12:02:26 +0200

    refactor: PakketService to .NET 6

--- pakketservice/Program.cs
+++ pakketservice/Program.cs
@@ -1,51 +1,83 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
+using PakketService.Database.Contexts;
+using PakketService.Database.Converters;
+using PakketService.Database.Datamodels;
+using PakketService.Database.Datamodels.Dtos;
+using PakketService.Services;
 
-namespace PakketService
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<KestrelServerOptions>(
+    builder.Configuration.GetSection("Kestrel"));
+
+builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+    options.AddPolicy(name: "_myAllowSpecificOrigins",
+        builder =>
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            builder
+            .WithOrigins("*")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin();
+        });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((context, services) =>
-                {
-                    services.Configure<KestrelServerOptions>(
-                        context.Configuration.GetSection("Kestrel"));
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureKestrel(serverOptions =>
-                    {
-                        serverOptions.Limits.MaxConcurrentConnections = 100;
-                        serverOptions.Limits.MaxConcurrentUpgradedConnections = 100;
-                        serverOptions.Limits.MaxRequestBodySize = 10 * 1024;
-                        serverOptions.Limits.MinRequestBodyDataRate =
-                            new MinDataRate(bytesPerSecond: 100,
-                                gracePeriod: TimeSpan.FromSeconds(10));
-                        serverOptions.Limits.MinResponseDataRate =
-                            new MinDataRate(bytesPerSecond: 100,
-                                gracePeriod: TimeSpan.FromSeconds(10));
-                        serverOptions.Limits.KeepAliveTimeout =
-                            TimeSpan.FromDays(30);
-                        serverOptions.Limits.RequestHeadersTimeout =
-                            TimeSpan.FromDays(30);
-                    })
-                    .UseStartup<Startup>()
-                    .UseUrls("http://*:5001"); //Add this line
-                });
-    }
+// Add context
+var connection = builder.Configuration.GetValue<string>("ConnectionString");
+builder.Services.AddDbContextPool<PackageServiceContext>(
+    options => options.UseSqlServer(connection));
+
+// Inject services.
+builder.Services.AddTransient<IPackageService, PackageService>();
+builder.Services.AddTransient<ITicketService, TicketService>();
+
+// Inject converters.
+builder.Services.AddScoped<IDtoConverter<Package, PackageRequest, PackageResponse>, DtoConverter>();
+builder.Services.AddScoped<IDtoConverter<Ticket, TicketRequest, TicketResponse>, TicketDtoConverter>();
+
+var app = builder.Build();
+
+// Migrate database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var addressContext = services.GetRequiredService<PackageServiceContext>();
+    addressContext.Database.Migrate();
 }
+
+if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+
+app.UseCors("_myAllowSpecificOrigins");
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSwagger(c =>
+{
+    c.SerializeAsV2 = true;
+});
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PakketService");
+    c.RoutePrefix = string.Empty;
+});
+
+
+app.Run();
